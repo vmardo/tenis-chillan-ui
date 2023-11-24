78a0acf52e4ec7e981fa967ccf34c9dc64221bc1
vmardo <alonso24mat@gmail.com>
2023-11-24 16:45:45 -0300

    manejo de formulario y errores en formulario de registro

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -3,10 +3,58 @@ import {
     Input,
     Button,
     Typography,
+    Alert,
   } from "@material-tailwind/react";
   import { NavLink } from "react-router-dom";
+  import { useState } from "react";
    
   export function Registro() {
+   //aca inicializamos useState como un array vacio 
+   //vamos hacer una propiedad por cada input y crearemos un objeto que contenga a los tres
+   //la idea es ir llenando segun lo que vaya escribiendo el usuario para ello debemos utilizar los eventos
+
+      const [usuario, setUsuario] = useState({
+          nombre: "",
+          correo: "",
+          password:""
+      })
+      //creamos un useState de error
+      const [error, setError] = useState();
+
+      
+
+      //Esta const es para manejar el evento onChange
+      const actualizarUsuario = (e) => {
+        const propiedad = e.target.name;
+        const valor = e.target.value 
+        //{...obj,key: "value"}
+        //con esta funcion estoy tomando todos los valores de los input 
+        setUsuario({...usuario, [propiedad]: valor})
+
+      
+      }
+      //preventDefault ---->para no perder los datos al momento de actualizar 
+      const registrarUsuario = (e)=> {
+        e.preventDefault();
+        //reealizaremos validaciones
+        console.log("tenemos que validar el objeto user:")
+        console.log(usuario);
+
+        //desenestructuramos el objeto para trabajar con las variables por separado
+        const {nombre,correo,password} = usuario;
+        //vamos hacer que todos los campos sean obligatorios
+        if (!nombre  || !correo  || !password ) {
+          setError("Todos los campos son obligatorios")
+          //return es para que no se ejecute mas codigo
+          return;
+        }
+
+        console.log("registrando usuario...")
+        //llamar a ruta encargada de registrar usuarios
+
+      }
+      
+
     return (
        <Card color="transparent" shadow={false}>
         <Typography variant="h4" color="blue-gray">
@@ -15,12 +63,14 @@ import {
         <Typography color="gray" className="mt-1 font-normal">
          Ingresa tus datos para registrarte 
         </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={registrarUsuario}>
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Tu nombre
             </Typography>
             <Input
+              name="nombre"
+              onChange={actualizarUsuario}
               size="lg"
               placeholder="tu nombre"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
@@ -32,6 +82,8 @@ import {
               Tu correo
             </Typography>
             <Input
+              name="correo"
+              onChange={actualizarUsuario}
               size="lg"
               placeholder="name@mail.com"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
@@ -43,6 +95,8 @@ import {
               Contraseña
             </Typography>
             <Input
+              name="password"
+              onChange={actualizarUsuario}
               type="text"
               size="lg"
               placeholder="********"
@@ -52,8 +106,15 @@ import {
               }}
             />
           </div>
-         
-          <Button className="mt-6" fullWidth>
+                {
+                  error && 
+                  <Alert color="red" className="my-4" variant="gradient">
+                    <span>{error} </span>
+                  </Alert>
+
+                }
+
+          <Button type="submit" className="mt-6" fullWidth>
             Registarse
           </Button>
           <Typography color="gray" className="mt-4 text-center font-normal">
